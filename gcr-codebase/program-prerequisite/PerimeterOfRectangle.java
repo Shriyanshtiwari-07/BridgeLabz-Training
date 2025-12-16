023fddcdc0311d1101a07939415fea25d3e2ee0c
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-16 21:07:10 +0530

    [Shriyansh] : Added PerimeterOfRectangle

---
+++ gcr-codebase/program-prerequisite/PerimeterOfRectangle.java
@@ -0,0 +1,25 @@
+package javaFundamentals.Week1.day1.Self.Question2;
+
+
+import java.util.Scanner;
+
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the length: ");
+        int length = sc.nextInt();
+
+        System.out.print("Enter the width: ");
+        int width = sc.nextInt();
+
+
+        int perimeter = 2 * (length+width);
+
+        System.out.println("Perimeter = " + perimeter);
+
+        sc.close();
+    }
+}
+
