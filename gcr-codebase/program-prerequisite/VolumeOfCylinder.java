e0ce9a5b7022f659e105c3de474eecb159d92c9c
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-16 20:44:57 +0530

    [Shriyansh] : Added VolumeOfCylinder

---
+++ gcr-codebase/program-prerequisite/VolumeOfCylinder.java
@@ -0,0 +1,21 @@
+package javaFundamentals.Week1.day1.Assisted.Question5;
+
+import java.util.Scanner;
+
+class VolumeOfCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("Volume of Cylinder = " + volume);
+
+        sc.close();
+    }
+}
+
