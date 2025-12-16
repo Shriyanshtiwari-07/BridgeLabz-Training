a84392e9d530afb8cdb1a0e59fe69064579510cb
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-16 21:17:38 +0530

    [Shriyansh] : Added KmToMiles

---
+++ gcr-codebase/program-prerequisite/KmToMiles.java
@@ -0,0 +1,18 @@
+package javaFundamentals.Week1.day1.Self.Question5;
+
+import java.util.Scanner;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Kilometers: ");
+        float km = sc.nextFloat();
+
+        float miles = km * 0.621371f;
+
+        System.out.println("Miles = " + miles);
+
+        sc.close();
+    }
+}
