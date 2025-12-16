6302a19260c5804a22723d14b96a64bf9383b9f0
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-16 16:48:43 +0530

    [Shriyansh] : Print first java program

---
+++ gcr-codebase/program-prerequisite/WelcomeToBridgeLabz.java
@@ -0,0 +1,8 @@
+package javaFundamentals.Week1.day1.Assisted.Question1;
+
+public class WelcomeToBridgeLabz{
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
