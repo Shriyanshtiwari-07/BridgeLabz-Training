6efc3ab31b36add9a948836354ae5217bb5dc79f
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-17 14:48:10 +0530

    [Shriyansh] : Added EarthVolume file

---
+++ gcr-codebase/programming-elements/Level 1/EarthVolume.java
@@ -0,0 +1,17 @@
+public class EarthVolume {
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        double pi = Math.PI;
+
+        double volumeKm3 = (4.0 / 3.0) * pi * Math.pow(radiusKm, 3);
+
+        // Conversion: 1 km = 0.621371 miles
+        double kmToMiles = 0.621371;
+        double volumeMiles3 = volumeKm3 * Math.pow(kmToMiles, 3);
+
+        System.out.println(
+            "The volume of earth in cubic kilometers is " + volumeKm3 +
+            " and cubic miles is " + volumeMiles3
+        );
+    }
+}
