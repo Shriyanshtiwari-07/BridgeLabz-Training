4d18466dc93a1cc00561166adf1567135304cfb8
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-17 15:31:53 +0530

    [Shriyansh] : Added FeeDiscountInput File

---
+++ gcr-codebase/programming-elements/Level 1/FeeDiscountInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FeeDiscountInput {
+    public static void main(String[] args) {
+        double fee;
+        double discountPercent;
+
+        Scanner input = new Scanner(System.in);
+
+        // Taking user input
+        fee = input.nextDouble();
+        discountPercent = input.nextDouble();
+
+        // Calculations
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+
+        System.out.println(
+            "The discount amount is INR " + discount +
+            " and final discounted fee is INR " + finalFee
+        );
+
+        input.close();
+    }
+}
