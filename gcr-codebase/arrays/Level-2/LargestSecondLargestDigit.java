300a0f952d6b887f49e1e962934a8c34c02b7ea3
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-20 00:58:47 +0530

    [Shriyansh] : Added LargestSecondLargestDigit file

---
+++ gcr-codebase/arrays/Level-2/LargestSecondLargestDigit.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class LargestSecondLargestDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        // Store digits in array
+        while (number != 0 && index < maxDigit) {
+            digits[index] = number % 10;
+            number = number / 10;
+            index++;
+        }
+
+        // Initialize largest and second largest
+        int largest = 0;
+        int secondLargest = 0;
+
+        // Find largest and second largest
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+
+        // Output
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+
+        sc.close();
+    }
+}
