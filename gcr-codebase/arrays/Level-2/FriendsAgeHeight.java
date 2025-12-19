0eb8e51de7db24ae1ddd94101771657eb82fd949
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-20 00:55:31 +0530

    [Shriyansh] : Added FriendsAgeHeight file

---
+++ gcr-codebase/arrays/Level-2/FriendsAgeHeight.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class FriendsAgeHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+
+        int[] age = new int[3];
+        double[] height = new double[3];
+
+        // Input
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            age[i] = sc.nextInt();
+
+            System.out.print("Enter height of " + names[i] + ": ");
+            height[i] = sc.nextDouble();
+        }
+
+        // Initialize youngest and tallest
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        // Find youngest and tallest
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Output
+        System.out.println("Youngest friend: " + names[youngestIndex]);
+        System.out.println("Tallest friend: " + names[tallestIndex]);
+
+        sc.close();
+    }
+}
