6c96fe0117a756e6981be50059d9a923137aa0eb
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-22 12:12:15 +0530

    [Shriyansh] : Added NumberChecker file

---
+++ gcr-codebase/Methods/Level-3/NumberChecker.java
@@ -0,0 +1,81 @@
+public class NumberChecker {
+
+    // Count digits
+    public static int countDigits(int number) {
+        int count = 0;
+        while (number != 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    // Store digits in array
+    public static int[] storeDigits(int number) {
+        int count = countDigits(number);
+        int[] digits = new int[count];
+
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+        return digits;
+    }
+
+    // Check Duck Number
+    public static boolean isDuckNumber(int[] digits) {
+        for (int d : digits) {
+            if (d != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check Armstrong Number
+    public static boolean isArmstrong(int number, int[] digits) {
+        int sum = 0;
+        int power = digits.length;
+
+        for (int d : digits) {
+            sum += Math.pow(d, power);
+        }
+        return sum == number;
+    }
+
+    // Find largest & second largest
+    public static void findLargest(int[] digits) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int d : digits) {
+            if (d > largest) {
+                secondLargest = largest;
+                largest = d;
+            } else if (d > secondLargest && d != largest) {
+                secondLargest = d;
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+        System.out.println("Second Largest: " + secondLargest);
+    }
+
+    // Find smallest & second smallest
+    public static void findSmallest(int[] digits) {
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int d : digits) {
+            if (d < smallest) {
+                secondSmallest = smallest;
+                smallest = d;
+            } else if (d < secondSmallest && d != smallest) {
+                secondSmallest = d;
+            }
+        }
+
+        System.out.println("Smallest: " + smallest);
+        System.out.println("Second Smallest: " + secondSmallest);
+    }
+}
