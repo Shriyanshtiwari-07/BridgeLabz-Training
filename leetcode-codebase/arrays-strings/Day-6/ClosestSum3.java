9d8ad62033db65c18977599c2d3fb1d264d31f32
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2025-12-30 01:01:33 +0530

    [Shriyansh] : Added ClosestSum3  file

---
+++ leetcode-codebase/arrays-strings/Day-6/ClosestSum3.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class ClosestSum3 {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                // Update closest sum if needed
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+                // Move pointers
+                if (currentSum < target) {
+                    left++;
+                } else if (currentSum > target) {
+                    right--;
+                } else {
+                    // Exact match found
+                    return target;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+}
