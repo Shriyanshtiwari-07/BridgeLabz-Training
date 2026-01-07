0a2f588263ca81fdee84fad6620f1139d19920d2
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-07 17:26:50 +0530

    [Shriyansh] : Added User file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/eventease/User.java
@@ -0,0 +1,16 @@
+package com.oops.eventease;
+
+
+public class User {
+	 private String name;
+	 private String email; // imp data
+
+	 public User(String name, String email) {
+		 this.name = name;
+		 this.email = email;
+	 }
+
+	 public String getName() {
+		 return name;
+	 }
+}
