910426ed510d2032df700597ffe6e800a8ce7a32
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-07 17:11:14 +0530

    [Shriyansh] : Added dog file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/petpal/Dog.java
@@ -0,0 +1,14 @@
+package com.oops.petpal;
+
+public class Dog extends Pet{
+
+	public Dog(String name, int age) {
+		super(name, age);
+		this.type = "Dog";
+	}
+	
+	@Override
+	public void makeSound() {
+		System.out.println(super.getName() + ", Bark Bark.....");
+	}
+}
