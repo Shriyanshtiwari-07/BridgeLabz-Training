a03cc8487ca33307931a9d82d3c90da65b14aadd
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-08 10:07:51 +0530

    [Shriyansh] : Added Vehicle file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/cabbyGo/Vehicle.java
@@ -0,0 +1,22 @@
+package com.oops.cabbyGo;
+
+
+public abstract class Vehicle extends Driver{
+	private String vehicleNumber;
+	private int capacity;
+	private String type;
+	
+	public Vehicle(String driverName, String licenseNo, float rating, String vehicleNumber, int capacity, String type) {
+		super(driverName, licenseNo, rating);
+		this.vehicleNumber = vehicleNumber;
+		this.capacity = capacity;
+		this.type = type;
+	}
+	
+	public void showVehicleDetails() {
+		System.out.println("-----------------------------------");
+		System.out.println("Vehicle Type: " + type);
+		System.out.println("Vehicle Number: " + vehicleNumber);
+		System.out.println("Capacity: " + capacity);
+	}
+}
