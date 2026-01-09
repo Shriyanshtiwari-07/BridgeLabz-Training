3b207c58b9f53b50b8403de5ac9fda33a76820e8
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-09 13:12:27 +0530

    [Shriyansh] : Added vehicle file

--- java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/cabbyGo/Vehicle.java
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/cabbyGo/Vehicle.java
@@ -1,22 +1,28 @@
 package com.oops.cabbyGo;
 
+public class Vehicle {
+    protected String driverName;
+    protected String licenseNo;
+    protected float rating;
+    protected String vehicleNumber;
+    protected int capacity;
+    protected String type;
 
-public abstract class Vehicle extends Driver{
-	private String vehicleNumber;
-	private int capacity;
-	private String type;
-	
-	public Vehicle(String driverName, String licenseNo, float rating, String vehicleNumber, int capacity, String type) {
-		super(driverName, licenseNo, rating);
-		this.vehicleNumber = vehicleNumber;
-		this.capacity = capacity;
-		this.type = type;
-	}
-	
-	public void showVehicleDetails() {
-		System.out.println("-----------------------------------");
-		System.out.println("Vehicle Type: " + type);
-		System.out.println("Vehicle Number: " + vehicleNumber);
-		System.out.println("Capacity: " + capacity);
-	}
+    public Vehicle(String driverName, String licenseNo, float rating, String vehicleNumber, int capacity, String type) {
+        this.driverName = driverName;
+        this.licenseNo = licenseNo;
+        this.rating = rating;
+        this.vehicleNumber = vehicleNumber;
+        this.capacity = capacity;
+        this.type = type;
+    }
+
+    public void showVehicleDetails() {
+        System.out.println("Driver: " + driverName);
+        System.out.println("License: " + licenseNo);
+        System.out.println("Rating: " + rating);
+        System.out.println("Vehicle No: " + vehicleNumber);
+        System.out.println("Capacity: " + capacity);
+        System.out.println("Type: " + type);
+    }
 }
