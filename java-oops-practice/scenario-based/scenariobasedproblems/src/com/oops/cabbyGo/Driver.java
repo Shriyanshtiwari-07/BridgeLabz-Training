8df2beff166af7b686c6569645cd7ba51ff3e88e
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-09 13:07:33 +0530

    [Shriyansh] : Added Driver file

--- java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/cabbyGo/Driver.java
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/cabbyGo/Driver.java
@@ -1,21 +1,25 @@
 package com.oops.cabbyGo;
 
 public class Driver {
-	
-	private String name;
-	private String licenseNumber;
-	private float rating;  // out of 5
-	
-	Driver(String name, String licenseNumber, float rating){
-		this.name = name;
-		this.licenseNumber = licenseNumber;
-		this.rating = rating;
-	}
-	
-	public void showDriverDetails() {
-		System.out.println("Driver Name: " + name);
-		System.out.println("Rating: " + rating);
-		System.out.println("-----------------------------------");
-	}
-	
+    private String name;
+    private String licenseNo;
+    private float rating;
+
+    public Driver(String name, String licenseNo, float rating) {
+        this.name = name;
+        this.licenseNo = licenseNo;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public float getRating() {
+        return rating;
+    }
 }
