e87be06ee63750a9fa1791ae82dfa7591fa82ad6
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-13 12:58:18 +0530

    [Shriyansh] : Added DiscountOrder file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/foodloop/DiscountOrder.java
@@ -0,0 +1,15 @@
+package com.oops.foodloop;
+
+
+public class DiscountOrder extends Order {
+
+    @Override
+    protected double applyDiscount(double total) {
+        if (total >= 1000) {
+            return total * 0.20;
+        } else if (total >= 500) {
+            return total * 0.10;
+        }
+        return 0;
+    }
+}
