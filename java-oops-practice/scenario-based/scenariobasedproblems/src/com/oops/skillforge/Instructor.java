c48e51e53d9e0b3ad5b4dc97ad452c3db5a15a8a
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-13 12:24:19 +0530

    [Shriyansh] : Added instructor file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/skillforge/Instructor.java
@@ -0,0 +1,12 @@
+package com.oops.skillforge;
+
+public class Instructor extends User {
+
+    public Instructor(String name, String email) {
+        super(name, email);
+    }
+
+    public void uploadCourse(Course course) {
+        System.out.println("Course uploaded: " + course.getTitle());
+    }
+}
