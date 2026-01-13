907e6f953f8ee31e75962e90b2e09b8d75511b1f
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-13 12:04:43 +0530

    [Shriyansh] : Added beginnerCOurse file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/skillforge/BeginnerCourse.java
@@ -0,0 +1,8 @@
+package com.oops.skillforge;
+
+public class BeginnerCourse extends Course {
+
+    public BeginnerCourse(String title, Instructor instructor) {
+        super(title, instructor);
+    }
+}
