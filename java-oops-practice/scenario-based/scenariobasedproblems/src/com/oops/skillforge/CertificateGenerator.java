1c3fee757b8c6a0085a33f6819e1267144c2ea44
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-13 12:22:56 +0530

    [Shriyansh] : Added COurse file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/skillforge/CertificateGenerator.java
@@ -0,0 +1,14 @@
+package com.oops.skillforge;
+
+
+public class CertificateGenerator {
+
+    public static String generate(Student student, Course course) {
+
+        if (course instanceof AdvancedCourse) {
+            return "Advanced Skill Certificate awarded to " + student.generateCertificate();
+        } else {
+            return "Beginner Skill Certificate awarded to " + student.generateCertificate();
+        }
+    }
+}
