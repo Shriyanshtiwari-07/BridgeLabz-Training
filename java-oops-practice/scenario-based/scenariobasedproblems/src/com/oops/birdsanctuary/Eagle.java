390a0706181c0c8dcc5e0c6a83f7dc2c3e27fcbd
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-18 22:08:50 +0530

    [Shriyansh] : Adde Eagle file

---
+++ java-oops-practice/scenario-based/scenariobasedproblems/src/com/oops/birdsanctuary/Eagle.java
@@ -0,0 +1,18 @@
+package com.oops.birdsanctuary;
+
+public class Eagle extends Bird implements Flyable {
+
+    public Eagle(String id, String name) {
+        super(id, name, "Eagle");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + " soars high in the sky.");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println(getId() + " | " + getName() + " | Eagle | Flyable");
+    }
+}
