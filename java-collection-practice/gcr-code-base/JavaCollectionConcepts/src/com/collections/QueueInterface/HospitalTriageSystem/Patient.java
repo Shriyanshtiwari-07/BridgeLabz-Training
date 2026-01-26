91c57416430048b96f737a6511988176c178e26e
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-01-26 13:49:42 +0530

    [SHriyansh] : Added Patient file

---
+++ java-collection-practice/gcr-code-base/JavaCollectionConcepts/src/com/collections/QueueInterface/HospitalTriageSystem/Patient.java
@@ -0,0 +1,13 @@
+package com.collections.QueueInterface.HospitalTriageSystem;
+
+import java.util.*;
+
+public class Patient {
+	public String name;
+	public int severity;
+	
+	public Patient(String name, int severity) {
+		this.name = name;
+		this.severity = severity;
+	}
+}
