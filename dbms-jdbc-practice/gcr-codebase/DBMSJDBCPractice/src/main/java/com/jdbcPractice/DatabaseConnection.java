d642aefeba1ae86016909ddeeea9499db7ef4567
Shriyanshtiwari-07 <tiwarishriyansh49@gmail.com>
2026-02-13 16:42:34 +0530

    [Shriyansh] : added Connection file

---
+++ dbms-jdbc-practice/gcr-codebase/DBMSJDBCPractice/src/main/java/com/jdbcPractice/DatabaseConnection.java
@@ -0,0 +1,42 @@
+package com.jdbcPractice;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+private static final String URL="jdbc:mysql://localhost:3306/employee_assignment";
+private static final String USER = "root";
+private static final String PASSWORD = "localhost";
+
+public static Connection getConnection() {
+	Connection con = null;
+	
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		con = DriverManager.getConnection(URL, USER, PASSWORD);
+		System.out.println("Database connected successfully");
+	}catch(ClassNotFoundException e) {
+		System.err.println("MySQL JDBC Driver not found!");
+		e.printStackTrace();
+	}
+	
+	catch(SQLException e) {
+		System.out.println("Connection Failed!");
+		e.printStackTrace();
+	}
+	return con;
+}
+public static void main(String[] args) {
+	Connection conn = getConnection();
+	if(conn !=null) {
+		try {
+			conn.close();
+			System.out.println("Connection closed.");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+ }
+}
